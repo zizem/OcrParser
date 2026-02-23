7f1106e905613366415929de43c35846e08a7e4f
zizem <zizem.2007@gmail.com>
2026-02-23 22:01:03 +0200

    screenShot

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,20 @@
 package org.example;
 
+import org.example.Screen.ScreenSelector;
+
+import javax.swing.*;
 import java.io.IOException;
 
 public class Main {
     static void main(String[] args) throws IOException {
-        String imagePath = "/home/zizen/Cource/JavaFinal/src/main/java/org/example/test/NewSeria.png";
-        String modelPath = "/home/zizen/Cource/JavaFinal/src/main/java/org/example/models/LapSRN_x8.pb";
+        String imagePath = "/home/zizen/Cource/JavaFinal/src/main/java/NewSeria.png";
+        String modelPath = "/home/zizen/Cource/JavaFinal/models/LapSRN_x8.pb";
         String outputPath = "/home/zizen/Cource/JavaFinal/src/main/java/org/example/output.png";
 
-        System.out.println("Starting image processing...");
-        try {
-            LinkCudaSO.CudaWrapperLib.INSTANCES_LINUX.processImage(imagePath, modelPath, outputPath);
 
-        } catch (Exception e) {
-            System.out.println("Error processing image" + e.getMessage());
-            e.printStackTrace();
-        }
+        SwingUtilities.invokeLater(() -> new ScreenSelector().setVisible(true));
+        System.out.println("Starting image processing...");
+
 
     }
 
--- src/main/java/org/example/ProcessImage.java
+++ src/main/java/org/example/ProcessImage.java
@@ -6,6 +6,5 @@ public class ProcessImage {
     public static void Process(String imagePath, String modelPath, String outputPath){
 
 
-
     }
 }
